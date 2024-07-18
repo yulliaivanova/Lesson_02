a902ea92407c2bd69789d22006da74b7d747fd8d
yuliiaivanova <gerda88823@gmail.com>
2024-07-19 00:04:51 +0200

    lesson3

---
+++ src/lesson_03/Main.java
@@ -0,0 +1,46 @@
+package lesson_03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Моя будущая собака");
+        int year = 2025;
+        System.out.println(year);
+
+        System.out.println("Вес будущей собаки");
+        double weight = 2.5;
+        System.out.println(weight);
+
+        System.out.println(" Стать собаки");
+        char sex = 'W';
+        System.out.println(sex);
+
+        System.out.println("Подарит мне мой парень собаку ?");
+        boolean gift = true;
+        System.out.println(gift);
+
+        System.out.println("Вероятность, что у меня будет собака");
+        float probabolityOfGift = 99.99f;
+        System.out.println(probabolityOfGift);
+
+        System.out.println("Мое желание получить собаку");
+        long wish = 1000000;
+        System.out.println(wish);
+
+        System.out.println("Вероятность получить собаку в 2024 году");
+        short probability2024 =0;
+        System.out.println(probability2024);
+
+        System.out.println("Моя готовность к появлению собаки");
+        byte ready = 1;
+        System.out.println(ready);
+
+
+
+
+
+
+
+
+    }
+}

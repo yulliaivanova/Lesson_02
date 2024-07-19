248039cac437dad9db8364bdf0f7306ea4e45d95
yuliiaivanova <gerda88823@gmail.com>
2024-07-19 15:42:32 +0200

    lesson 4

---
+++ src/lesson_04/Calculator.java
@@ -0,0 +1,16 @@
+package lesson_04;
+
+public class Calculator {
+
+
+    int number1;
+    int number2;
+
+    public Calculator( int number1, int number2){
+        this.number1 = number1;
+        this.number2 = number2;
+
+    }
+
+
+}
---
+++ src/lesson_04/Main.java
@@ -0,0 +1,16 @@
+package lesson_04;
+
+public class Main {
+    public static void main(String[] args) {
+//        Calculator calculator = new Calculator();
+//        calculator.number1 = 25;
+//        calculator.number2 = 37;
+
+        Calculator calculator = new Calculator(25, 37)
+
+        System.out.println("Выводим цифры");
+        System.out.println(calculator);
+
+
+    }
+}

a97aec623d93dcf370f046ecf9ecd80e4e721d12
yuliiaivanova <gerda88823@gmail.com>
2024-07-22 08:10:30 +0200

    corrected homework 4

--- src/lesson_04/Calculator.java
+++ src/lesson_04/Calculator.java
@@ -6,10 +6,28 @@ public class Calculator {
     int number1;
     int number2;
 
-    public Calculator( int number1, int number2){
+    public Calculator(int number1, int number2) {
         this.number1 = number1;
         this.number2 = number2;
 
+    }
+
+    public int getSumOfNumbers() {
+        int result = number1 + number2;
+        return result;
+    }
+
+    public int getSubtractNumbers() {
+        int result1= number1 - number2;
+        return result1;
+    }
+    public int getMultiplication () {
+        int result2 = number1 * number2;
+        return result2;
+    }
+    public  double getDivideNumbers () {
+        double result3 = number2 / number1;
+        return  result3;
     }
 
 
--- src/lesson_04/Main.java
+++ src/lesson_04/Main.java
@@ -6,11 +6,24 @@ public class Main {
 //        calculator.number1 = 25;
 //        calculator.number2 = 37;
 
-        Calculator calculator = new Calculator(25, 37)
+        Calculator calculator = new Calculator(25, 37);
 
+          int result =calculator.getSumOfNumbers();
+          int result1 = calculator.getSubtractNumbers();
+          int result2 = calculator.getMultiplication();
+          double result3 = calculator.getDivideNumbers();
+//
         System.out.println("Выводим цифры");
-        System.out.println(calculator);
+        System.out.println(calculator.number1);
+        System.out.println(calculator.number2);
+        System.out.println("Вывести сумму:" + result);
+        System.out.println("Вывести разность:" + result1);
+        System.out.println("Вывести произведение:" + result2);
+        System.out.println("Вывести частное:" + result3);
+
 
 
     }
+
+
 }

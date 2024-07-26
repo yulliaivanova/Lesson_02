4e1f76d0850cc1f9ffd0e2479c7147e1456da596
yuliiaivanova <gerda88823@gmail.com>
2024-07-26 13:23:34 +0200

    Homework 6

--- src/lesson_06/task_01/Main.java
+++ src/lesson_06/task_01/Main.java
@@ -1,11 +1,50 @@
 package lesson_06.task_01;
 
 public class Main {
-    int a = 5;
-    int b = 7;
-    int c = 3;
+    public static void main(String[] args) {
+      int houseNummer = 4;
+      String adressee = " Ivanov";
 
-//    public static String triangleType = getTriangleType(a,b,c);
+        String text = " Письмо доставлено в  ";
+        String text1 = " Письмо не доставлено, неверный адрес ";
+
+        switch (houseNummer){
+            case 1 :
+                System.out.println(text + " первый дом");
+                break;
+            case 2:
+                System.out.println(text + " второй дом");
+                break;
+            case 3:
+                System.out.println(text + " третий дом");
+                break;
+            case 4:
+                System.out.println(text + " в четвертый дом семье ");
+
+             switch (adressee) {
+                 case "Ивановы":
+                     System.out.println(text + " Ивановых");
+                     break;
+                 case "Петровы":
+                     System.out.println(text + " Петровых");
+                     break;
+             }
+                 System.out.println( adressee.length() % 2 == 0? " четное колиество символов" : "нечетное количество символов");
+                 case 5:
+                     System.out.println(text + " в пятый дом");
+                     break;
+                 default:
+                     System.out.println(text1 );
+                     break;
+             }
+
+        }
+
+    }
 
 
-}
+
+
+
+
+
